f614d0b009666eafbd4bff82269867b315b0a819
yagiz545467 <unluleyla52@gmail.com>
2026-02-21 22:48:59 +0300

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'turk-red': '#E30A17',
+      }
+    },
+  },
+  plugins: [],
+}
